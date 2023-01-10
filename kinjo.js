8ed9589c6c89315708442d375dd676451cea09dd
Daiki Iizuka <da.iz.mg@gmail.com>
2023-01-10 17:06:36 +0900

    スクボ以外に普通のウェブサイトを登録できるようにする (#1)
    * add readme
    
    * comments
    
    * my data
    
    * ざっくり対応

--- kinjo.js
+++ kinjo.js
@@ -10,10 +10,11 @@ function loadScript (url) {
         document.body.appendChild(script)
     })
 }
-async function loadAll() {
-    await loadScript('https://scrapbox.io/api/code/masui/POI/poi.js')
-}
-    
+
+// async function loadAll() {
+//     await loadScript('https://scrapbox.io/api/code/masui/POI/poi.js')
+// }
+
 $(function(){
     //loadAll()
     // setTimeout(function(){ alert(data[2]) }, 5000)
@@ -30,14 +31,28 @@ function distance(lat1, lng1, lat2, lng2) {
 }
 
 var locations = []
-for(let i=0; i<data.length; i++){
-    let m = data[i].match(/\[\/(.*)\/(.*)\]\s+.*@([\d\.]+),([\d\.]+),(\d+)z/)
-    entry = {}
-    entry.project = m[1]
-    entry.title = m[2]
-    entry.latitude = m[3]
-    entry.longitude = m[4]
-    entry.zoom = m[5]
+for (let i = 0; i < data.length; i++){
+    const entry = {}
+    // Scrapbox project URL
+    const  m = data[i].match(/\[\/(.*)\/(.*)\]\s+.*@([\d\.]+),([\d\.]+),(\d+)z/)
+    // Webpage URL
+    const  m1 = data[i].match(/\[(https?:\/\/.*)\s(.*)\]\s+.*@([\d\.]+),([\d\.]+),(\d+)z/)
+
+    if (m) {
+        entry.project = m[1]
+        entry.title = m[2]
+        entry.latitude = m[3]
+        entry.longitude = m[4]
+        entry.zoom = m[5]
+    } else if (m1) {
+        entry.project = null
+        entry.url = m1[1]
+        entry.title = m1[2]
+        entry.latitude = m1[3]
+        entry.longitude = m1[4]
+        entry.zoom = m1[5]
+    }
+    console.log(entry)
     locations.push(entry)
 }
 
@@ -45,26 +60,30 @@ var curlatitude, curlongitude
 
 function calc(){
     for(var i=0;i<locations.length;i++){
-	entry = locations[i]
-	entry.distance = distance(entry.latitude,entry.longitude,curlatitude,curlongitude)
+        entry = locations[i]
+        entry.distance = distance(entry.latitude,entry.longitude,curlatitude,curlongitude)
     }
     locations.sort((a, b) => { // 近い順にソート
-	return a.distance > b.distance ? 1 : -1;
+        return a.distance > b.distance ? 1 : -1;
     });
     // alert(locations[0].distance)
     for(var i=0;i<10;i++){
-	let loc = locations[i]
-	let li = $('<li>')
-	let e = $('<a>')
-	e.text(loc.title)
-	e.attr('href',`https://scrapbox.io/${loc.project}/${loc.title}`)
-	li.append(e)
-	li.append($('<span>').text(' '))
-	let map = $('<a>')
-	map.text('map')
-	map.attr('href',`https://www.google.com/maps/@${loc.latitude},${loc.longitude},${loc.zoom}z`)
-	li.append(map)
-	$('#list').append(li)
+        let loc = locations[i]
+        let li = $('<li>')
+        let e = $('<a>')
+        e.text(loc.title)
+        if (loc.project) {
+            e.attr('href', `https://scrapbox.io/${loc.project}/${loc.title}`)
+        } else {
+            e.attr('href', loc.url)
+        }
+        li.append(e)
+        li.append($('<span>').text(' '))
+        let map = $('<a>')
+        map.text('map')
+        map.attr('href',`https://www.google.com/maps/@${loc.latitude},${loc.longitude},${loc.zoom}z`)
+        li.append(map)
+        $('#list').append(li)
     }
 }
 
@@ -94,3 +113,5 @@ function errorCallback(error) {
     alert(err_msg)
     //document.getElementById("show_result").innerHTML = err_msg;
 }
+
+// https://scrapbox.io/daiiz/POI
